21eb19cfe9b47cc58900ec7671cf9d890e0ab5e8
Matt Avery <mavery@akamai.com>
2015-07-27 09:21:59 -0400

    better output in test app

--- main.c
+++ main.c
@@ -5,8 +5,11 @@
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
   int i;
-  for(i=0; i<argc; i++){
-    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+  while(i < argc){
+    printf("%s = %s", azColName[i], argv[i] ? argv[i] : "NULL");
+    while(++i < argc){
+      printf(", %s = %s", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
   }
   printf("\n");
   return 0;
@@ -31,14 +34,19 @@ int main(int argc, char **argv){
     return 1;
   }
     
-  rc = sqlite3_exec(db, "CREATE VIRTUAL TABLE proc_env USING env", 0, 0, 0);
+  rc = sqlite3_exec(db, "CREATE VIRTUAL TABLE env USING env", 0, 0, 0);
   if( rc!=SQLITE_OK ){
     fprintf(stderr,"error creating virtual table with rc %i\n",rc);
     return 1;
   }
 
-  printf("%s\n",argv[1]);
-  rc = sqlite3_exec(db, argv[1], callback, 0, &zErrMsg);
+  char default_query[] = "SELECT * from env";
+  char* query = default_query;
+  if(argc < 1) {
+    query = argv[1];
+  }
+  printf("query: %s\n",query);
+  rc = sqlite3_exec(db, query, callback, 0, &zErrMsg);
   if( rc!=SQLITE_OK ){
     fprintf(stderr, "SQL error: %s\n", zErrMsg);
     sqlite3_free(zErrMsg);
